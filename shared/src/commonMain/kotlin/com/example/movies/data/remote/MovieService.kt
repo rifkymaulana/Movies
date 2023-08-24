c86fe9afed7597f179f3df88d50e2d275563e83a
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-08-24 21:39:37 +0700

    feat: dashboard and detail

--- shared/src/commonMain/kotlin/com/example/movies/data/remote/MovieService.kt
+++ shared/src/commonMain/kotlin/com/example/movies/data/remote/MovieService.kt
@@ -5,7 +5,6 @@ import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 
 internal class MovieService: KtorApi() {
-
     suspend fun getMovies(page: Int = 1): MoviesResponse = client.get {
         pathUrl("movie/popular")
         parameter("page", page)
